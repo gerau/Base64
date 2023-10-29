58d0a6d6a5d9002e1090b75d20edc53813a999bf
Volodymyr Herashchenko <cholkegn@gmail.com>
2023-10-29 15:18:11 +0200

    finished file decode method

--- Base64/Encoder.cs
+++ Base64/Encoder.cs
@@ -17,62 +17,58 @@ namespace Base64
             }
             if (fileNameOut.Length == 0) fileNameOut = fileNameIn.Remove(fileNameIn.LastIndexOf('.'));
             var l = fileNameIn.Substring(fileNameIn.LastIndexOf('.'));
-            if (!l.Contains("txt"))
+            if (l != ".txt")
             {
                 Console.WriteLine("Error: input file not text file.");
                 return;
             }
 
-            using (FileStream fs = File.OpenRead(fileNameIn))
+            using FileStream fs = File.OpenRead(fileNameIn);
+            using StreamWriter writetext = new($"{fileNameOut}.base64");
+            var size = fs.Length / 57;
+            var coded = string.Empty;
+            byte[] temp = new byte[57];
+            for (int i = 0; i < size; i++)
             {
-                using (StreamWriter writetext = new StreamWriter($"{fileNameOut}.base64"))
+                fs.Read(temp, 0, 57);
+
+                for (int j = 0; j < 19; j++)
                 {
-                    var size = fs.Length / 57;
-                    var coded = string.Empty;
-                    byte[] temp = new byte[57];
-                    for (int i = 0; i < size; i++)
-                    {
-                        fs.Read(temp, 0, 57);
-
-                        for (int j = 0; j < 19; j++)
-                        {
-                            byte[] buf = { temp[3 * j], temp[3 * j + 1], temp[3 * j + 2] };
-                            coded += Convertor.EncodeTriplet(buf);
-                        }
-                        writetext.WriteLine(coded);
-
-                        coded = string.Empty;
-                        temp = new byte[57];
-                    }
-
-                    fs.Read(temp, 0, 57);
-                    size = fs.Length % 57;
-                    for (int i = 0; i < size / 3; i++)
-                    {
-                        byte[] buf = { temp[3 * i], temp[3 * i + 1], temp[3 * i + 2] };
-                        coded += Convertor.EncodeTriplet(buf);
-                    }
-                    switch (size % 3)
-                    {
-                        case 0:
-                            break;
-                        case 1:
-                            byte[] buf1 = { temp[size / 3] };
-                            coded += Convertor.EncodeSymbol(buf1);
-                            break;
-                        case 2:
-                            byte[] buf2 = { temp[size / 3], temp[size / 3 + 1] };
-                            coded += Convertor.EncodeSymbol(buf2);
-                            break;
-                    }
-
-                    writetext.WriteLine(coded);
-
-                    writetext.Close();
-                    fs.Close();
+                    byte[] buf = { temp[3 * j], temp[3 * j + 1], temp[3 * j + 2] };
+                    coded += Convertor.EncodeTriplet(buf);
                 }
+                writetext.WriteLine(coded);
+
+                coded = string.Empty;
+                temp = new byte[57];
             }
+
+            fs.Read(temp, 0, 57);
+            size = fs.Length % 57;
+            for (int i = 0; i < size / 3; i++)
+            {
+                byte[] buf = { temp[3 * i], temp[3 * i + 1], temp[3 * i + 2] };
+                coded += Convertor.EncodeTriplet(buf);
+            }
+            switch (size % 3)
+            {
+                case 0:
+                    break;
+                case 1:
+                    byte[] buf1 = { temp[size - 1] };
+                    coded += Convertor.EncodeSymbol(buf1);
+                    break;
+                case 2:
+                    byte[] buf2 = { temp[size - 2], temp[size - 1] };
+                    coded += Convertor.EncodeDuplet(buf2);
+                    break;
+            }
+            writetext.WriteLine(coded);
+
+            writetext.Close();
+            fs.Close();
         }
+
         public static void DecodeFile(string fileNameIn, string fileNameOut = "")
         {
             if (!File.Exists(fileNameIn))
@@ -81,61 +77,134 @@ namespace Base64
                 return;
             }
             if (fileNameOut.Length == 0) fileNameOut = fileNameIn.Remove(fileNameIn.IndexOf('.'));
-            var l = fileNameIn.Substring(fileNameIn.IndexOf("."));
+            var l = fileNameIn.Substring(fileNameIn.LastIndexOf("."));
+            if (l != ".base64")
+            {
+                Console.WriteLine("Error: input file not .base64 file.");
+                return;
+            }
             try
             {
                 string[] lines = File.ReadAllLines(fileNameIn);
                 try
                 {
-                    using (StreamWriter writetext = new StreamWriter($"{fileNameOut}.txt"))
+                    using StreamWriter writetext = new($"{fileNameOut}");
+                    var lastLine = 0;
+                    var write = "";
+                    for (int i = 0; i < lines.Length; i++)
                     {
-                        var counter = 1;
-                        for (int i = 0; i < lines.Length; i++)
+                        if (lines[i][0] == '-')
                         {
-                            if (lines[i][0] == '-')
-                            {
-                                continue;
-                            }
-                            if (((lines[i].Length % 4) != 0) | (lines[i].Length > 76))
-                            {
-                                Console.WriteLine($"Line{i + 1}: Incorrect length {lines[i].Length}");
-                            }
-                            if (lines[])
+                            continue;
                         }
-                        for (int i = 0; i < lines.Length - 1; i++)
+                        if (((lines[i].Length % 4) != 0) | (lines[i].Length > 76))
                         {
-                            for(int j = 0; j < 19; j++)
+                            Console.WriteLine($"Line {i + 1}: Incorrect length {lines[i].Length}");
+                            return;
+                        }
+                        lastLine = i;
+                        if ((lines[i][lines[i].Length - 1] == '=') & (lines.Length != i + 1))
+                        {
+                            for(int j = lastLine + 1; j < lines.Length; j++)
                             {
-                                byte[] buf = new byte[4];
-                                string str = lines[i].Substring(j * 4, j * 4 + 3);
-                                var err = Convertor.DecodeTriplet(str, buf);
-                                if(err != 0)
+                                if (lines[j][0] != '-')
                                 {
-                                    Console.WriteLine($"Line{i+1}, Pos{j*4 + err - 1}: Incorrect symbol '{lines[i][j * 4 + err - 1]}' ");
-                                    return;
+                                    Console.WriteLine("Warning: available data after the end of the message");
+                                    break;
                                 }
                             }
                         }
-
-
-
-
-
-                        //writetext.WriteLine();
-
-                        writetext.Close();
                     }
+                    for (int i = 0; i < lastLine; i++) 
+                    {
+                        if (lines[i][0] == '-' | lastLine == i) continue;
+
+                        if (lines[i].Length != 76)
+                        {
+                            Console.WriteLine($"Line {i + 1}: Incorrect length - expected 76, but {lines[i].Length}");
+                            return;
+                        }
+
+                        if (lines[i].Contains('='))
+                        {
+                            Console.WriteLine($"Line {i + 1}: Incorrect symbol '=' - expected on end of the line {lastLine + 1}");
+                            return;
+                        }
+
+                        for (int j = 0; j < 19; j++)
+                        {
+                            byte[] buf = new byte[3];
+                            string str = lines[i].Substring(j * 4,4);
+                            var err = Convertor.DecodeTriplet(str, buf);
+                            if (err != 0)
+                            {
+                                Console.WriteLine($"Line{i + 1}, Pos{j * 4 + err}: Incorrect symbol '{lines[i][j * 4 + err]}' ");
+                                return;
+                            }
+                            write = System.Text.Encoding.Default.GetString(buf);
+                            writetext.Write(write);
+                        }
+                    }
+                    string s = lines[lastLine].TrimEnd('=');
+                    for (int j = 0; j < s.Length / 4; j++)
+                    {
+                        byte[] buf = new byte[3];
+                        string str = s.Substring(j * 4,4);
+                        var err = Convertor.DecodeTriplet(str, buf);
+                        if (s.Contains('='))
+                        {
+                            Console.WriteLine($"Line {lastLine + 1}: Incorrect symbol '=' - expected on end of the line {lastLine + 1}");
+                            return;
+                        }
+                        if (err != 0)
+                        {
+                            Console.WriteLine($"Line {lastLine + 1}, Pos{j * 4 + err - 1}: Incorrect symbol '{s[j * 4 + err - 1]}' ");
+                            return;
+                        }
+                        write = System.Text.Encoding.Default.GetString(buf);
+                        writetext.Write(write);
+                    }
+                    var k = 0; var error = 0; var subs = "";
+                    switch (s.Length % 4)
+                    {
+                        case 3:
+                            byte[] buf = new byte[2];
+                            k = s.Length - 3;
+                            subs = s.Substring(k);
+                            error = Convertor.DecodeDuplet(subs, buf);
+                            if (error != 0)
+                            {
+                                Console.WriteLine($"Line {lastLine + 1}, Pos{k + error}: Incorrect symbol '{s[k + error]}' ");
+                                return;
+                            }
+                            write = System.Text.Encoding.Default.GetString(buf);
+                            writetext.Write(write);
+                            break;
+                        case 2:
+                            buf = new byte[1];
+                            k = s.Length - 2;
+                            subs = s.Substring(k);
+                            error = Convertor.DecodeSymbol(subs, buf);
+                            if (error != 0)
+                            {
+                                Console.WriteLine($"Line {lastLine + 1}, Pos{k + error}: Incorrect symbol '{s[k + error]}' ");
+                                return;
+                            }
+                            write = System.Text.Encoding.Default.GetString(buf);
+                            writetext.Write(write);
+                            break;
+                    }
+                    writetext.Close();
                 }
                 catch (Exception ex)
                 {
                     
-                    Console.WriteLine($"Error: cannot create the file{fileNameOut}.base64 - {ex.ToString()}");
+                    Console.WriteLine($"Error: cannot create the file {fileNameOut}.base64 - {ex}");
                 }
-                
             }
             catch(Exception ex)
             {
-                Console.WriteLine($"Error: cannot open the file{fileNameIn} - {ex.ToString()}");
+                Console.WriteLine($"Error: cannot open the file{fileNameIn} - {ex}");
             }
         }
     }
