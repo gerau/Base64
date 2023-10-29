a6e627dad1dee1b99062360c0b4d536f58f2558f
Volodymyr Herashchenko <cholkegn@gmail.com>
2023-10-29 15:18:20 +0200

    some testing

--- Base64/Program.cs
+++ Base64/Program.cs
@@ -6,10 +6,33 @@ namespace Base64
     {
         static void Main(string[] args)
         {
-            byte[] bytes = { 0xFF, 0xFF, 0xFF };
-            Console.WriteLine(Convertor.EncodeTriplet(bytes));
-            Console.WriteLine(Convertor.EncodeDuplet(bytes));
-            Console.WriteLine(Convertor.EncodeSymbol(bytes));
+            byte[] bytes = { 
+                0xCF,
+                0xF1,
+                0x2E };
+            var triplet = Convertor.EncodeTriplet(bytes);
+            var duplet = Convertor.EncodeDuplet(bytes);
+            var symbol = Convertor.EncodeSymbol(bytes);
+
+            Console.WriteLine($"triplet = {triplet}");
+            Console.WriteLine($"duplet = {duplet}");
+            Console.WriteLine($"symbol = {symbol}");
+
+            var result = new byte[3];
+            Convertor.DecodeTriplet("ZQ==", result);
+            Console.WriteLine($"decoded triplet = {result[0].ToString("X")}, {result[1].ToString("X")}, {result[2].ToString("X")}");
+            Console.WriteLine(System.Text.Encoding.Default.GetString(result));
+            result = new byte[2];
+            Convertor.DecodeDuplet(duplet, result);
+            Console.WriteLine($"decoded duplet = {result[0]:X}, {result[1].ToString("X")}");
+            result = new byte[1];
+            Convertor.DecodeSymbol(symbol, result);
+            Console.WriteLine($"decoded symbol = {result[0]:X}");
+
+            //Encoder.EncodeFile("C:\\data\\test.txt");
+
+            Encoder.DecodeFile("C:\\data\\test.base64", "C:\\data\\test2.txt");
+            Console.ReadLine();
         }
     }
 }
